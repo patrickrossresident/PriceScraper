fcc6e5aaf382e49286eed7b401bd3ec232406587
= <serj.sova@gmail.com>
2021-03-05 15:44:53 +0200

    updated port

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const cheerio = require("cheerio")
 
 dotenv.config();
 
-const port = parseInt(process.env.PORT, 10) || 3000;
+const port = parseInt(process.env.PORT, 10) || 5000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
